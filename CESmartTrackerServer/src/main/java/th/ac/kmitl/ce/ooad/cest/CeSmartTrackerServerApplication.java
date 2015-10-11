d84ebe8c5f3e218f467a9771298dd45d1518fa72
atsawa17 <s6010962@kmitl.ac.th>
2015-10-11 17:25:32 +0700

    Initial server project with simple create and find course

---
+++ CESmartTrackerServer/src/main/java/th/ac/kmitl/ce/ooad/cest/CeSmartTrackerServerApplication.java
@@ -0,0 +1,12 @@
+package th.ac.kmitl.ce.ooad.cest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeSmartTrackerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeSmartTrackerServerApplication.class, args);
+    }
+}
