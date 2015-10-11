c41cf93329fc46a4117b944e93be0e4d54d42b52
atsawa17 <s6010962@kmitl.ac.th>
2015-10-11 21:56:42 +0700

    Change create course responses

--- CESmartTrackerServer2/src/main/java/th/ac/kmitl/ce/ooad/cest/controller/course/CreateCourseController.java
+++ CESmartTrackerServer2/src/main/java/th/ac/kmitl/ce/ooad/cest/controller/course/CreateCourseController.java
@@ -10,28 +10,37 @@ import th.ac.kmitl.ce.ooad.cest.config.AppConfig;
 import th.ac.kmitl.ce.ooad.cest.dao.ICourseDao;
 import th.ac.kmitl.ce.ooad.cest.entity.Course;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/createCourse")
 public class CreateCourseController {
 
     @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody
-    Course request(
+    public @ResponseBody String request(
             @RequestParam(value="courseId", required=true) String courseId,
             @RequestParam(value="courseName", required=true) String courseName) {
         return saveCourse(courseId, courseName);
     }
 
-    public Course saveCourse(String courseId, String courseName) {
-        Course course = new Course();
-        course.setCourseId(courseId);
-        course.setCourseName(courseName);
+    public String saveCourse(String courseId, String courseName) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
         ICourseDao edao = ctx.getBean(ICourseDao.class);
-        edao.saveCourse(course);
-        return course;
-    }
 
+        List<Course> courses = edao.findByCourseId(courseId);
+        if(courses.isEmpty())
+        {
+            Course course = new Course();
+            course.setCourseId(courseId);
+            course.setCourseName(courseName);
+            edao.saveCourse(course);
+            return "success";
+        }
+        else
+        {
+            return "duplicatedCourseId";
+        }
+    }
 }
